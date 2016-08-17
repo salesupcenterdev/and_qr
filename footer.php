512fb7629fccb9aa232a31837dafdb84d15789ae
k-marianna <k-marianna@ukr.net>
2016-08-17 16:30:42 +0300

    нові файли для підключення в головній сторінці

---
+++ footer.php
@@ -0,0 +1,12 @@
+<section id="footer" class="footer">
+		<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<a href="http://facebook.com/autonovad" target="_blank"><i class="fa fa-facebook fa-icon" aria-hidden="true"></i>Мы в Facebook</a><br>
+						Copyright &copy; <a href="http://autonovad.ua" target="_blank">AutoNovaD</a>, 2016
+					</div>
+				</div>
+				<br>
+		</div>
+		<!-- /.container -->
+</section> 
---
+++ navigation.php
@@ -0,0 +1,38 @@
+
+<!-- Navigation -->
+<!-- Navigation -->
+	<nav class="navbar navbar-fixed-top navbar-light" role="navigation">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img class="nav-logo" src="img/logo.png"></a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="index.php" class="navbar-nav-item">Главная</a>
+					</li>
+					<li>
+						<a href="#about" class="navbar-nav-item">Про сервис</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" id="dLabel" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="navbar-nav-item">Сервисы<span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="dLabel">
+								<li><a href="evaq.html">Список эвакуаторов</a></li>
+								<li><a href="taxi.html">Список такси</a></li>
+							</ul>
+					</li>
+					<li>
+						<a href="#contacts" class="navbar-nav-item">Контакты</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		<!-- /.container -->
+	</nav>
