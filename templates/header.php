d5750e94c4bfc779f0f5326532233cdd806a8f8a
salesupcenterdev <zoring@salesupcenter.com>
2016-08-31 12:08:55 +0300

    add one-page-wonder.css to header

--- templates/header.php
+++ templates/header.php
@@ -30,6 +30,9 @@
 	<!-- Custom CSS -->
 	<link href="./../css/style.css" rel="stylesheet">
 
+	<!-- Custom CSS -->
+	<link href="./../css/one-page-wonder.css" rel="stylesheet">
+
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
